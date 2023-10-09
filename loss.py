3bb70569e1e0e0f6ad54eeb645e2d88506b18d1a
Or Haifler <99145615+OrHaifler@users.noreply.github.com>
2023-10-09 15:34:23 +0300

    Update loss.py

--- loss.py
+++ loss.py
@@ -2,11 +2,12 @@ import numpy as np
 
 
 def cross_entropy(yh, y):
-
+    #yh: (20,4)
+    #y: (20,1)
     N = yh.shape[0]
-    yhn = np.exp(yh - yh.max(axis=1))
-    scores = np.exp(yhn) / np.exp(yhn).sum(axis = 1)
-    loss = -(np.log(scores)).mean()
+    yhn = np.exp(yh - yh.max(axis = 1).reshape(N, 1))
+    scores = np.exp(yhn) / np.exp(yhn).sum(axis = 1).reshape(N,1)
+    loss = -(np.log(scores[np.arange(N), y])).mean()
     scores[np.arange(N), y] -= 1
     dX = scores / N
     return loss, dX
@@ -25,7 +26,7 @@ def svm(yh, y, delta=1):
 def MSE(yh, y):
 
     N = yh.shape[0]
-    diff = yh - yh[np.arange(N), y]
+    diff = yh - y.reshape(N,1)
     loss = (diff ** 2).sum() / (2 * N)
     dX = 1 / N * diff
     return loss, dX
