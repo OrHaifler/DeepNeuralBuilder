68e10b445f15e571f0eac187efdfe8aeeefd26c0
Or Haifler <99145615+OrHaifler@users.noreply.github.com>
2023-10-09 15:33:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,105 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from initializer import initialize
 from layers import *
+from functions_utils import *
+from loss import *
+
+
+class Layer:
+
+    def __init__(self, layer):
+        self.weights = {}
+        self.weights['W'], self.weights['b'] = initialize(layer)
+        self.forward, self.backward = function_utils[layer['type']]
+
 class NN:
     #Initialize a neural network
-    def __init__(self, layers):
-        self.params = {}
-        #self.init_functions = {"fc": "fc_initialize"}
-        self.L = len(layers)
-
+    def __init__(self, layer_array):
+        self.types = ['fc', 'conv', 'dropout', 'batchnorm', 'max_pooling',
+                      'average_pooling', 'softmax', 'relu', 'sigmoid', 'tanh']
+        self.L = len(layer_array)
+        self.layers = []
         for i in range(self.L):
-                    #Write an init_method that will initialize parameters
-                    self.params[f'W{i}'], self.params[f'b{i}']  = initialize(layers[i])
+            self.layers.append(Layer(layer_array[i]))
+        self.caches = {}
+        self.grads = {}
 
 
-    #Define forward method for each layer type
+    def single_forward(self, X: np.ndarray, layer: Layer) -> (np.ndarray, tuple):
 
+        out, cache = layer.forward(X, layer.weights['W'], layer.weights['b'])
+        return out, cache
 
+    def forward(self, X, y, loss_method):
 
+        out = X
+        for i in range(self.L):
+            out, self.caches[i] = self.single_forward(out, self.layers[i])
+        return loss_method(out, y)
 
+    def predict(self, X):
 
-X = np.random.randint(5, size = (3,3))
-layers = [{'dim': (3,2), 'ext': (1, ), 'init_method': 'random_normal'}, {'dim': (2,1), 'ext': (1, ), 'init_method': 'random_normal'}]
-Test = NN(layers)
-W1, b1 = Test.params['W0'], Test.params['b0']
-W2, b2 = Test.params['W1'], Test.params['b1']
-Z1 = fc_forward(X, W1, b1)
-Z2 = fc_forward(Z1, W2, b2)
-print(Z2)
-
-
-
-
-
-
-
-
+        out = X
+        for i in range(self.L):
+            out, self.caches[i] = self.single_forward(out, self.layers[i])
+        return out
 
 
 
+    def train(self, X, y, loss_method, lr=1e-3, clipping_threshold=10000,  epochs=100000):
 
+        N = X.shape[0]
+        self.history = []
+
+        for epoch in range(epochs):
+
+            loss, self.grads[self.L] = self.forward(X, y, loss_method)
+            self.grads[self.L] = [self.grads[self.L]]
+            for l in range(self.L - 1, -1, -1):
+                self.grads[l] = fc_backward(loss, self.grads[l + 1][0], self.caches[l])
+                dX, dW, db = self.grads[l]
+                dW_norm, db_norm = np.linalg.norm(dW), np.linalg.norm(db)
+                if dW_norm > clipping_threshold:
+                    dW *= clipping_threshold / dW_norm
+                if db_norm > clipping_threshold:
+                    db *= clipping_threshold / db_norm
+                self.layers[l].weights['W'] -= lr * dW / N
+                self.layers[l].weights['b'] -= lr * db / N
+            if epoch % 1000 == 0:
+                self.history.append(loss)
+                print(epoch, loss)
+                print("--------------")
+        plt.plot(self.history)
+        plt.show()
+
+
+
+
+
+#Test example
+X = np.random.randint(0, 10, size=(50,4))
+y = X.sum(axis = 1)
+layers_arr = [{'type': 'fc', 'dim': (4,64), 'ext': (1, ), 'init_method': 'random_normal'},
+            {'type': 'fc', 'dim': (64,32), 'ext': (1, ), 'init_method': 'random_normal'},
+              {'type': 'fc', 'dim': (32,1), 'ext': (1, ), 'init_method': 'random_normal'}]
+Network = NN(layers_arr)
+Network.train(X, y, MSE, epochs=10000)
+print(Network.predict(np.array([1, 5, 20, 3])))
+
+
+
+
+'''dX2, dW2, db2 = fc_backward(loss, dloss, self.caches[1])
+dX1, dW1, db1 = fc_backward(loss, dX2, self.caches[0])
+self.layers[1].weights['W'] -= lr * dW2 / X.shape[0]
+self.layers[1].weights['b'] -= lr * db2 / X.shape[0]
+self.layers[0].weights['W'] -= lr * dW1 / X.shape[0]
+self.layers[0].weights['b'] -= lr * db1 / X.shape[0]
+self.history.append((epoch, loss))
+if epoch % 1000 == 0:
+    print(epoch, loss)
+    print("--------------")'''
 
 
 
